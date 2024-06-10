d1964ab98174fea6e52b7333eadf8ce8319f8cce
Matt <73149745+Deathbringer98@users.noreply.github.com>
2024-06-10 20:48:13 -0230

    Add files via upload

---
+++ src/main/java/com/keyin/Node.java
@@ -0,0 +1,8 @@
+package com.keyin;
+
+public class Node {
+    public int value;
+    public Node next = null;
+}
+
+// Author: Matthew Menchinton
---
+++ src/main/java/com/keyin/SingleLinkedList.java
@@ -0,0 +1,113 @@
+package com.keyin;
+
+public class SingleLinkedList {
+    public Node head;
+    public Node tail;
+    public int size;
+
+    public Node createLinkedList(int nodeValue) {
+        head = new Node();
+        Node node = new Node();
+        node.next = null;
+        node.value = nodeValue;
+        head = node;
+        tail = node;
+        size = 1;
+        return head;
+    }
+
+    public void insertInLinkedList(int nodeValue, int location) {
+        Node node = new Node();
+        node.value = nodeValue;
+        if (head == null) {
+            createLinkedList(nodeValue);
+            return;
+        } else if (location == 0) {
+            node.next = head;
+            head = node;
+        } else if (location >= size) {
+            tail.next = node;
+            node.next = null;
+            tail = node;
+        } else {
+            Node tempNode = head;
+            int index = 0;
+            while (index < location - 1) {
+                tempNode = tempNode.next;
+                index++;
+            }
+            node.next = tempNode.next;
+            tempNode.next = node;
+        }
+        size++;
+    }
+
+    public void traverseLinkedList() {
+        if (head == null) {
+            System.out.println("Linked List does not exist!");
+        } else {
+            Node tempNode = head;
+            for (int i = 0; i < size; i++) {
+                System.out.print(tempNode.value);
+                if (i != size - 1) {
+                    System.out.print(" -> ");
+                }
+                tempNode = tempNode.next;
+            }
+        }
+        System.out.print("\n");
+    }
+
+    public boolean searchNode(int nodeValue) {
+        if (head != null) {
+            Node tempNode = head;
+            for (int i = 0; i < size; i++) {
+                if (tempNode.value == nodeValue) {
+                    System.out.println("Found the node: " + tempNode.value + " at location: " + i + "\n");
+                    return true;
+                }
+                tempNode = tempNode.next;
+            }
+        }
+        System.out.println("Node not found");
+        return false;
+    }
+
+    public boolean deleteNode(int location) {
+        if (head == null) {
+            System.out.println("SLL does not exist");
+            return false;
+        } else if (location == 0) {
+            head = head.next;
+            size--;
+            if (size == 0) {
+                tail = null;
+            }
+            return true;
+        } else if (location >= size) {
+            Node tempNode = head;
+            for (int i = 0; i < size - 1; i++) {
+                tempNode = tempNode.next;
+            }
+            if (tempNode == head) {
+                head = tail = null;
+                size--;
+                return true;
+            }
+            tempNode.next = null;
+            tail = tempNode;
+            size--;
+            return true;
+        } else {
+            Node tempNode = head;
+            for (int i = 0; i < location - 1; i++) {
+                tempNode = tempNode.next;
+            }
+            tempNode.next = tempNode.next.next;
+            size--;
+            return true;
+        }
+    }
+}
+
+// Author: Matthew Menchinton
